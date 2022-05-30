7430bebda4fe1b38d3ad6bb55b0aba3281563fed
Walter <silvawalteralipio@gmail.com>
2022-05-30 12:43:03 -0300

    separando as interfaces

---
+++ UNIP-calc/src/types/inputFields.ts
@@ -0,0 +1,6 @@
+export interface InputFields{
+  name: string,
+  id: string,
+  value: string,
+  setValue:  React.Dispatch<React.SetStateAction<string>>
+}
---
+++ UNIP-calc/src/types/propsMedias.ts
@@ -0,0 +1,6 @@
+import { setValue } from "./setValue";
+
+
+export interface propsMedias {
+  calcMedia: ({valueAva, valuePim, valueProva}: setValue)=> void
+}
